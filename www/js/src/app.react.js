b65092dda761ee7bb8151faf218b4e34a086fa50
Bruce Lefebvre <brucelefebvre@gmail.com>
2015-09-05 16:28:38 -0400

    Refactored DiaperEventForm code to separate it from the rest of the page content

--- www/js/src/app.react.js
+++ www/js/src/app.react.js
@@ -172,74 +172,6 @@ var DiaperEvent = React.createClass({
 });
 
 
-var DiaperEventForm = React.createClass({
-	// Navigation mixin required to use ReactRouter's `transitionTo(..)`
-	mixins: [Navigation],
-	onDateUpdate: function(date) {
-		this.setState({
-			date: date
-		});
-	},
-	handleSubmit: function(e) {
-		e.preventDefault();
-
-		this.firebaseRef = new Firebase(firebaseURI + "/events/");
-
-		var eventDetails = {
-			type: "Diaper",
-			pee: React.findDOMNode(this.refs.pee).checked,
-			poo: React.findDOMNode(this.refs.poo).checked,
-			date: this.state.date.getTime(),
-			notes: React.findDOMNode(this.refs.notes).value
-		}
-
-		this.firebaseRef.push(eventDetails, function complete(error) {
-			if (error) { 
-				return alert( "Firebase error: [" + error + "]"); 
-			}
-		
-			// Return to root once submission is complete
-			this.transitionTo('/');
-		}.bind(this));
-	},
-	render: function() {
-		return (
-			<div>
-				<Header title="Diaper Event"/>
-
-				<div className="content has-header">
-					<form className="eventForm" onSubmit={this.handleSubmit}>
-						<h4>When?</h4>
-						<TimeSelector onUpdate={this.onDateUpdate}/>
-
-						<h4>What?</h4>
-						<ul className="list">
-							<li className="item item-checkbox">
-								<label className="checkbox">
-									<input type="checkbox" ref="pee" />
-								</label>
-								Pee
-							</li>
-							<li className="item item-checkbox">
-								<label className="checkbox">
-									<input type="checkbox" ref="poo" />
-								</label>
-								Poo
-							</li>
-						</ul>
-
-						<h4>Notes</h4>
-						<textarea ref="notes" placeholder="Details..." />
-
-						<button className="button button-positive" type="submit">Save</button>
-					</form>
-				</div>
-			</div>
-		);
-	}
-});
-
-
 var FoodEvent = React.createClass({
 	render: function() {
 		return (
@@ -276,13 +208,32 @@ var WakeEvent = React.createClass({
 	}
 });
 
+var AddDiaperEvent = React.createClass({
+	// Navigation mixin required to use ReactRouter's `transitionTo(..)`
+	mixins: [Navigation],
+	diaperEventAdded: function() {
+		// Return to root
+		this.transitionTo('/');
+	},
+	render: function() {
+		return (
+			<div>
+				<Header title="Diaper Event"/>
+				<div className="content has-header">
+					<DiaperEventForm successCallback={this.diaperEventAdded} />
+				</div>
+			</div>
+		);
+	}
+});
+
 // Enable touch events
 React.initializeTouchEvents(true);
 
 // Define the app's routes
 var routes = (
 	<Route name="app" path="/" handler={App}>
-		<Route name="diaperEvent" handler={DiaperEventForm}/>
+		<Route name="diaperEvent" handler={AddDiaperEvent}/>
 		<Route name="foodEvent" handler={FoodEvent}/>
 		<Route name="sleepEvent" handler={SleepEvent}/>
 		<Route name="wakeEvent" handler={WakeEvent}/>
--- www/js/src/forms.react.js
+++ www/js/src/forms.react.js
@@ -49,6 +49,65 @@ var TimeSelector = React.createClass({
 	}
 });
 
+
+var DiaperEventForm = React.createClass({
+	onDateUpdate: function(date) {
+		this.setState({
+			date: date
+		});
+	},
+	handleSubmit: function(e) {
+		e.preventDefault();
+
+		this.firebaseRef = new Firebase(firebaseURI + "/events/");
+
+		var eventDetails = {
+			type: "Diaper",
+			pee: React.findDOMNode(this.refs.pee).checked,
+			poo: React.findDOMNode(this.refs.poo).checked,
+			date: this.state.date.getTime(),
+			notes: React.findDOMNode(this.refs.notes).value
+		}
+
+		this.firebaseRef.push(eventDetails, function complete(error) {
+			if (error) { 
+				return alert( "Firebase error: [" + error + "]"); 
+			}
+			// Fire success callback
+			this.props.successCallback();
+		}.bind(this));
+	},
+	render: function() {
+		return (
+			<form className="eventForm" onSubmit={this.handleSubmit}>
+				<h4>When?</h4>
+				<TimeSelector onUpdate={this.onDateUpdate}/>
+
+				<h4>What?</h4>
+				<ul className="list">
+					<li className="item item-checkbox">
+						<label className="checkbox">
+							<input type="checkbox" ref="pee" />
+						</label>
+						Pee
+					</li>
+					<li className="item item-checkbox">
+						<label className="checkbox">
+							<input type="checkbox" ref="poo" />
+						</label>
+						Poo
+					</li>
+				</ul>
+
+				<h4>Notes</h4>
+				<textarea ref="notes" placeholder="Details..." />
+
+				<button className="button button-positive" type="submit">Save</button>
+			</form>
+		);
+	}
+});
+
 var CheckboxList = React.createClass({
 	render: function() {
 		return (
